f3c3d6fbfc308149448c2750fd23b132daad4bc6
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-08-13 12:42:01 -0700

    I have finally made a self sustaning Makefile!

--- intro2algo_book/01chapter_introduction/deps/array.d
+++ intro2algo_book/01chapter_introduction/deps/array.d
@@ -1,4 +1,4 @@
 build/array.o: srcs/array.c hdrs/array.h
-	gcc srcs/array.c -Wall -Wpedantic -g -I hdrs  -c -o  build/array.o
+	gcc srcs/array.c -ansi -std=c99 -Wall -Wpedantic -g -I hdrs  -c -o  build/array.o
 
 hdrs/array.h:
--- intro2algo_book/01chapter_introduction/deps/main.d
+++ intro2algo_book/01chapter_introduction/deps/main.d
@@ -1,6 +1,6 @@
 build/main.o: srcs/main.c hdrs/mysort.h hdrs/insertion_sort.h \
  hdrs/sorttest.h hdrs/array.h
-	gcc srcs/main.c -Wall -Wpedantic -g -I hdrs  -c -o  build/main.o
+	gcc srcs/main.c -ansi -std=c99 -Wall -Wpedantic -g -I hdrs  -c -o  build/main.o
 
 hdrs/mysort.h:
 
