14a8563623e65ed8b9412fb6456235796e4efbed
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-08-13 18:23:36 -0700

    Added src file extension variable
    Although not really needed for this build
    You can imagine that it would be useful when for
    example changing to C++

--- intro2algo_book/01chapter_introduction/deps/insertion_sort.d
+++
@@ -1,4 +0,0 @@
-build/insertion_sort.o: srcs/insertion_sort.c hdrs/insertion_sort.h
-	gcc srcs/insertion_sort.c -ansi -std=c99 -Wall -Wpedantic -g -I hdrs  -c -o  build/insertion_sort.o
-
-hdrs/insertion_sort.h:
--- intro2algo_book/01chapter_introduction/deps/sorttest.d
+++
@@ -1,6 +0,0 @@
-build/sorttest.o: srcs/sorttest.c hdrs/sorttest.h hdrs/array.h
-	gcc srcs/sorttest.c -ansi -std=c99 -Wall -Wpedantic -g -I hdrs  -c -o  build/sorttest.o
-
-hdrs/sorttest.h:
-
-hdrs/array.h:
