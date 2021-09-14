320815952c5901a61f4081c8b1ef6d257edb7250
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-09-13 19:45:54 -0700

    Almost done with the bag class

--- Assignments/06BagContainerClass/bag.cpp
+++ Assignments/06BagContainerClass/bag.cpp
@@ -61,19 +61,80 @@ Bag<E>& Bag<E>::operator =(const Bag<E>& that)
 template<typename E>
 void Bag<E>::insert(const E& value)
 {
+    uint64_t valueRelativeOffset_InArray;
+    bool valueIs_IN_InternalArray = false;  //default assuming "value"
+                                            //is not in private "array"
 
+    if (size == capacity)
+    {
+        //only have to check for valueIs_IN_InternalArray if resize occurs
+        //since when capcity is increase a whole new block of memory
+        //is allocated and the address of "value" is no longer valid
+        //since the old block is deleted
+        valueIs_IN_InternalArray = isInArray(value);
+        if (valueIs_IN_InternalArray)
+            valueRelativeOffset_InArray = inArray_RelativeOffset(value);
+        //use a relative offset to avoid copying
+        doubleCapcity();
+    }
+
+    array[size++] = (valueIs_IN_InternalArray) ? array[valueRelativeOffset_InArray] : value;
 }
 //EOF
 
 template<typename E>
+bool Bag<E>::erase(const E& target)
+{
+    uint64_t shift_left_amount = 0; //goes up every time the target
+                                    //is encountered, and by default
+                                    //no targets have been found
+    
+    for (uint64_t i = 0; i < size; i++)
+    {
+        //point to the next value that is the not equal to the target, and
+        //count the amount of successive values that equal to the target
+        while (i < size && array[i] == target) 
+            {i++; shift_left_amount++;} //notice that "i" is also being incremeneted
+        //does the actual shifting
+        array[i - shift_left_amount] = array[i];
+    }
+
+    //remember, shift_left_amount also tells us
+    //how many targets we encountered since in the
+    //while loop we incremented shift_left_amount
+    //every single time that array[i] == target
+    //therefore we have shift_left_amount less values
+    this->size -= shift_left_amount;
+
+    return shift_left_amount; //if 0 then false, else true
+}
+//EOF
+
+template<typename E>
+bool Bag<E>::erase_one(const E& target)
+{
+    uint64_t positionOfTarget = 0;
+    for (; positionOfTarget < size && array[positionOfTarget] != target; positionOfTarget++) {}
+
+    if (positionOfTarget == size) //went through the whole array, no instance of target
+        return false;
+    
+    for (int i = positionOfTarget + 1; i < size; i++)
+        array[i - 1] = array[i];
+
+    size -= 1;
+    return true;
+}
+
+template<typename E>
 void Bag<E>::setCapacity(uint64_t new_capacity)
 {
-    E* temp_array = new E[new_capacity];
+    E* temp_array = new E[new_capacity]();
 
     capacity = new_capacity;
     for (uint64_t i = 0; i < size; i++)
         temp_array[i] = array[i];
-    
+
     delete [] array;
     array = temp_array;
 }
@@ -88,4 +149,28 @@ void Bag<E>::doubleCapcity(void)
         setCapacity(capacity*2);
 }
 //EOF
-#endif
+
+template<typename E>
+inline bool Bag<E>::isInArray(const E& value)
+{
+    //although array + capacity will also determine if inside
+    //the array, array + size might be safer since only allowing
+    //addresses that have already been populated
+    return (&value >= array) && (&value < (array + size));
+}
+
+template<typename E>
+inline uint64_t Bag<E>::inArray_RelativeOffset(const E& value)
+{
+    return (&value) - array;
+}
+
+template<typename E>
+std::ostream& operator <<(std::ostream& out, const Bag<E>& a_bag)
+{
+    for (int i = 0; i < a_bag.size(); i++)
+        out << a_bag[i];
+    return out;
+}
+
+#endif //BAG_CLASS_IMPLEMENTATION
--- Assignments/06BagContainerClass/bag.h
+++ Assignments/06BagContainerClass/bag.h
@@ -2,6 +2,7 @@
 #define BAG_CLASS_H
 
 #include <cstdint>
+#include <iostream>
 
 template<typename E>
 class Bag
@@ -16,20 +17,27 @@ public:
     bool erase(const E& target); //remove all instances of target
     bool erase_one(const E& target); //remove the first instance of the target
     Bag<E>& operator +=(const Bag<E>&that);
+    Bag<E>& operator [](uint64_t index);
 
     void count(const E& target) const; //returns the numbers of instances of target
     void size(void) const; //returns the number of elements stored
     void capacity(void) const; //returns max size of internal array
     Bag<E> operator +(const Bag<E>&that) const;
+    const Bag<E>& operator [](uint64_t index) const;
 
 private:
     void setCapacity(uint64_t new_capacity);
     void doubleCapcity(void);
+    inline bool isInArray(const E& value);
+    inline uint64_t inArray_RelativeOffset(const E& value);
 
     uint64_t size;
     uint64_t capacity;
     E* array;
 };
+
+template<typename E>
+std::ostream& operator <<(std::ostream& out, const Bag<E>& a_bag);
 
 #include "bag.cpp"
 
